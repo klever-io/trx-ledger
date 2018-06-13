07b2a136afdaac895c388a60965dfa8cb81d0fe0
CryptoSpaces <cryptospaces1@gmail.com>
2018-06-13 11:22:16 +0200

    Commented and formatted scripts.

--- test_getPublicKey.py
+++ test_getPublicKey.py
@@ -18,23 +18,28 @@ def parse_bip32_path(path):
 	return result
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--path', help="BIP 32 path to retrieve")
+parser.add_argument('--path', help="BIP32 path to retrieve. e.g. \"44'/195'/0'/0/0\".")
 args = parser.parse_args()
 
 if args.path == None:
 	args.path = "44'/195'/0'/0/0"
 
 donglePath = parse_bip32_path(args.path)
+
+# Create APDU message.
+# CLA 0x27
+# INS 0x02  GET_PUBLIC_KEY
+# P1 0x00   NO USER CONFIRMATION
+# P2 0x00   NO CHAIN CODE
 # Ask for confirmation
 # txt = "27020100" + '{:02x}'.format(len(donglePath) + 1) + '{:02x}'.format( int(len(donglePath) / 4 / 2)) + donglePath
 # No confirmation
-txt = "27020000" + '{:02x}'.format(len(donglePath)+1) + '{:02x}'.format( int(len(donglePath) / 4 / 2)) + donglePath
-apdu = bytearray.fromhex(txt)
-
-
+apduMessage = "27020000" + '{:02x}'.format(len(donglePath) + 1) + '{:02x}'.format(int(len(donglePath) / 4 / 2)) + donglePath
+apdu = bytearray.fromhex(apduMessage)
 
 print("-= Tron Ledger =-")
 print("Request Public Key")
+
 dongle = getDongle(True)
 result = dongle.exchange(apdu)
 size=result[0]
@@ -42,6 +47,7 @@ if size == 65 :
 	print("Public Key: " + result[1:1+size].hex())
 else:
 	print("Error... Public Key Size: {:d}".format(size))
+
 size=result[size+1]
 if size == 34 :
 	print("Address: " + result[67:67+size].decode())
@@ -49,7 +55,3 @@ if size == 34 :
 		print("Address match with test case!")
 else:
 	print("Error... Address Size: {:d}".format(size))
-
-
-
-
--- test_getVersion.py
+++ test_getVersion.py
@@ -2,9 +2,17 @@
 
 from ledgerblue.comm import getDongle
 
+# Create APDU message.
+# CLA 0x27
+# INS 0x06  GET_APP_CONFIGURATION
+# P1 0x00   NO USER CONFIRMATION
+# P2 0x00   NO CHAIN CODE
+apduMessage = "27060000ff"
+
 print("-= Tron Ledger =-")
 print("Request app Version")
-dongle = getDongle(True)
-result = dongle.exchange(bytearray.fromhex("27060000FF"))
-print('Version={:d}.{:d}.{:d}'.format(result[1],result[0],result[3]))
 
+dongle = getDongle(True)
+result = dongle.exchange(bytearray.fromhex(apduMessage))
+
+print('Version={:d}.{:d}.{:d}'.format(result[1],result[0],result[3]))
